0b4db0df9d84ce1f703f860c3545e7c35ddcd5c9
108598005 <eva111021@gmail.com>
2020-04-13 18:34:39 +0800

    first_commit

---
+++ 郭1/src/test/java/domain/usecase/CreateWorkFlowTest.java
@@ -0,0 +1,21 @@
+package domain.usecase;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CreateWorkFlowTest {
+    @Test
+    public void createWorkflow(){
+        CreateWorkflowUseCase createworkflowUseCase = new CreateWorkflowUseCase();
+        CreateWorkflowInput createworkflowInput = new CreateWorkflowInput();
+        CreateWorkflowOutput createworkflowOutput = new CreateWorkflowOutput();
+
+        
+
+        assertEquals("","123");
+
+    }
+
+
+}

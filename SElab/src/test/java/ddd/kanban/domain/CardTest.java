15eaaf1b1a80203a3787632631f54f38523ed4dc
陳冠穎 <t108598034@ntut.org.tw>
2020-05-05 22:30:08 +0800

    Add CardTest (DomainEvent Test)

---
+++ SElab/src/test/java/ddd/kanban/domain/CardTest.java
@@ -0,0 +1,21 @@
+package ddd.kanban.domain;
+
+import ddd.kanban.domain.model.card.Card;
+import org.junit.Test;
+
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
+
+public class CardTest {
+    @Test
+    public void testCreateCardShouldGenerateCardCreatedDomainEvent() {
+        String cardId = UUID.randomUUID().toString();
+        String boardId = UUID.randomUUID().toString();
+        String workflowId = UUID.randomUUID().toString();
+        Card card = new Card(cardId, "CardName", "Card Description", boardId, workflowId);
+
+        assertEquals(1, card.getDomainEvents().size());
+        card.clearDomainEvents();
+    }
+}

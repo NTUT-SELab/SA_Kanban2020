6c0159335d9731d5f5455177f6b9f6b06462d9ff
enchang <enchang1017@gmail.com>
2020-05-07 19:43:39 +0800

    modify package userinterface to view

--- SElab/src/main/java/ddd/kanban/view/Kanban.java
+++ SElab/src/main/java/ddd/kanban/view/Kanban.java
@@ -1,11 +1,9 @@
-package ddd.kanban.userinterface;
+package ddd.kanban.view;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;

bb82560d0d5bf3433df65850b1551ac9e2323b5c
Kai Huang <kai.huang4b6169@gmail.com>
2020-05-13 00:11:34 +0800

    vue front end initial

---
+++ SElab/KanbanFront/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <navBar/>
+    <div v-vk-margin>
+      <vk-button>Button</vk-button>
+      <vk-button disabled>Disabled</vk-button>
+      <vk-button-link href="#">Link</vk-button-link>
+    </div>
+    <div uk-lightbox>
+    <a class="uk-button uk-button-default" href="images/photo.jpg">Open Lightbox</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import navBar from './components/navBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    navBar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ SElab/KanbanFront/src/components/navBar.vue
@@ -0,0 +1,13 @@
+<template>
+<vk-navbar>
+  <vk-navbar-nav slot="left">
+    <vk-iconnav-item href="#" icon="home"></vk-iconnav-item>
+  </vk-navbar-nav>
+
+  <vk-navbar-logo slot="center">Kanban</vk-navbar-logo>
+
+  <vk-navbar-nav slot="right">
+    <vk-iconnav-item href="#" icon="plus"></vk-iconnav-item>
+  </vk-navbar-nav>
+</vk-navbar>
+</template>

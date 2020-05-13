0f90807661a3dd367f54b62a9c42fdbfe3b6572e
enchang <enchang1017@gmail.com>
2020-05-13 16:49:13 +0800

    front end finish

--- SElab/KanbanFront/src/components/BoardCard.vue
+++ SElab/KanbanFront/src/components/BoardCard.vue
@@ -1,52 +1,91 @@
 <template>
 <div class="board-view">
-    <vk-grid gutter="medium" class="uk-child-width-1-5@m uk-text-center">
-        <div>
-            <vk-card id="boardCard" padding="large" hover>
-            <vk-card-title id="cardTitle">Default board</vk-card-title>
-            </vk-card>
+    <div id="createBoardButton">
+        <vk-button type="primary" id="navImageButton" @click="show = true"><img src="https://i.imgur.com/T50ORBG.png"></vk-button>
+    </div>
+    <div id="grid">
+        <vk-grid gutter="medium" class="uk-child-width-1-5@m uk-text-center">
+            <div  v-for="(board) in boards" :key = board >
+                <vk-card id="boardCard" padding="large" hover>
+                <vk-card-title id="cardTitle">{{ board.boardTitle }}</vk-card-title>
+                </vk-card>
+            </div>
+        </vk-grid>
+    </div>
+    
+
+      <vk-modal center :show.sync="show">
+        <div class="container">
+            <form>
+                <fieldset class="uk-fieldset">
+                    <vk-modal-close @click="show = false"></vk-modal-close>
+                    <legend id="modalLegend" class="uk-legend">Create Board</legend>
+
+                    <div class="uk-margin">
+                        <input id="modalBoardTitle" class="uk-input" type="text" placeholder="Board Title" v-model="boardTitle">
+                    </div>
+
+                    <div class="uk-margin">
+                        <textarea class="uk-textarea" rows="5" placeholder="Board Description" v-model="boardDescription"></textarea>
+                    </div>
+                    <p v-vk-margin>
+                        <vk-button type="primary" id="modalCreateButton" @click="createBoard">Create</vk-button>
+                    </p>
+                </fieldset>
+            </form>
         </div>
-        <div>
-            <vk-card id="boardCard" padding="large" hover>
-            <vk-card-title id="cardTitle">Default board</vk-card-title>
-            </vk-card>
-        </div>
-        <div>
-            <vk-card id="boardCard" padding="large" hover>
-            <vk-card-title id="cardTitle">Default board</vk-card-title>
-            </vk-card>
-        </div>
-        <div>
-            <vk-card id="boardCard" padding="large" hover>
-            <vk-card-title id="cardTitle">Default board</vk-card-title>
-            </vk-card>
-        </div>
-        <div>
-            <vk-card id="boardCard" padding="large" hover>
-            <vk-card-title id="cardTitle">Default board</vk-card-title>
-            </vk-card>
-        </div>
-        <div>
-            <vk-card id="boardCard" padding="large" hover>
-            <vk-card-title id="cardTitle">Default board</vk-card-title>
-            </vk-card>
-        </div>
-        <div>
-            <vk-card id="boardCard" padding="large" hover>
-            <vk-card-title id="cardTitle">Default board</vk-card-title>
-            </vk-card>
-        </div>
-        
-    </vk-grid>
+    </vk-modal>
+  <vk-notification status="primary" :messages.sync="messages"></vk-notification>
 </div>
 
 </template>
 
+<script>
+export default {
+    data(){
+        return{
+            show: false,
+            messages: [],
+            boards: [],
+            boardTitle: "",
+            boardDescription: ""
+        }
+    },
+    methods:{
+        clearModalForm: function(){
+            this.boardTitle = "";
+            this.boardDescription = "";
+        },
+        createBoard: function(){
+            // axios.post('/api',{
+            //     boardTitle: this.boardTitle,
+            //     boardDescription: this.boardDescription
+            // }).then(function (response) {
+            //     this.boards.push({boardTitle: response.boardTitle})
+            //     this.messages.push({ message: 'Success', status: 'success' });
+            // })
+            // .catch(function (error) {
+            //     console.log(error);
+            //     this.messages.push({ message: 'Fail', status: 'Danger' });
+            // });
+            this.show = false;
+            this.messages.push({ message: 'Success', status: 'success' });
+            this.boards.push({boardTitle: this.boardTitle})
+            this.clearModalForm();
+        }
+    }
+}
+</script>
+
 <style scoped>
     .board-view{
-        padding-top: 60px;
+        padding-top: 20px;
         padding-left: 300px;
         padding-right: 60px;
+    }
+
+    #grid{
+        padding-top: 20px;
     }
 
     #boardCard{
@@ -56,5 +95,18 @@
     #cardTitle{
         color: white;
         font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif, bold;
+    }
+
+    #createBoardButton{
+        padding-left: 95%;
+    }
+
+    #modalLegend{
+        font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif, bold;
+    }
+
+    #modalCreateButton{
+        color: white;
+        font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
     }
 </style>
--- SElab/KanbanFront/src/components/navBar.vue
+++ SElab/KanbanFront/src/components/navBar.vue
@@ -8,36 +8,10 @@
     <vk-navbar-logo id="nvaLogo" slot="center">Kanban</vk-navbar-logo>
 
     <vk-navbar-nav slot="right">
-      <vk-button type="primary" id="navImageButton" @click="show = true"><img src="https://i.imgur.com/T50ORBG.png"></vk-button>
       <vk-button type="primary" id="navAvatarButton"><img src="https://i.imgur.com/lojmoSO.png"></vk-button>
     </vk-navbar-nav>
 
   </vk-navbar>
-
-  <vk-modal center :show.sync="show">
-    <div class="container">
-        <form>
-            <fieldset class="uk-fieldset">
-                <vk-modal-close @click="show = false"></vk-modal-close>
-                <legend id="modalLegend" class="uk-legend">Create Board</legend>
-
-                <div class="uk-margin">
-                    <input class="uk-input" type="text" placeholder="Board Title">
-                </div>
-
-                <div class="uk-margin">
-                    <textarea class="uk-textarea" rows="5" placeholder="Board Description"></textarea>
-                </div>
-                <p v-vk-margin>
-                    <vk-button type="primary" id="modalCreateButton" @click="createBoard">Create</vk-button>
-                </p>
-            </fieldset>
-        </form>
-    </div>
-  </vk-modal>
-
-  <vk-notification status="primary" :messages.sync="messages"></vk-notification>
-
 </div>
 
 </template>
@@ -55,18 +29,12 @@ export default {
     createBoard: function(){
       this.show = false;
       this.messages.push({ message: 'Success', status: 'success' });
-      this.messages.push({ message: 'Fail', status: 'danger' });
     }
   }
 }
 </script>>
 
 <style scoped>
-
-  #navInput{
-    background-color: transparent;
-  }
-
   #navHomeButton{
     background-color: transparent;
   }
@@ -75,28 +43,15 @@ export default {
     background-color: transparent;
   }
 
-  #navImageButton{
-    background-color: transparent;
-  }
-
   #boardNav{
     background-color: #23a8fa !important;
     height: 80px;
-  }
-
-  #modalCreateButton{
-    color: white;
-    font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
   }
 
   #nvaLogo{
     color: white;
     font-size: 40px;
     font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-  }
-
-  #modalLegend{
-    font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif, bold;
   }
 
 </style>

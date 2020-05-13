74af70b0e780a907f32d3f559e3e18b3049b2500
Kai Huang <kai.huang4b6169@gmail.com>
2020-05-13 09:26:03 +0800

    add board with dialog

--- SElab/KanbanFront/src/components/navBar.vue
+++ SElab/KanbanFront/src/components/navBar.vue
@@ -9,13 +9,36 @@
 
     <vk-navbar-nav slot="right">
       <vk-iconnav-item icon="plus" v-on:click="openBoardModal"></vk-iconnav-item>
+      <vk-button @click="show = true">New</vk-button>
     </vk-navbar-nav>
 
   </vk-navbar>
 
-  <div>
+  <div >
       <modal-board v-model="boardModalOpen"></modal-board>
   </div>
+
+  <vk-modal size="container" :show.sync="show">
+    <div class="container">
+        <form>
+            <fieldset class="uk-fieldset">
+
+                <legend class="uk-legend">Create Board</legend>
+
+                <div class="uk-margin">
+                    <input class="uk-input" type="text" placeholder="Board Title">
+                </div>
+
+                <div class="uk-margin">
+                    <textarea class="uk-textarea" rows="5" placeholder="Board Description"></textarea>
+                </div>
+                <p v-vk-margin>
+                    <vk-button @click.prevent="show = false">close</vk-button>
+                </p>
+            </fieldset>
+        </form>
+    </div>
+  </vk-modal>
 
 </div>
 
@@ -29,6 +52,7 @@ export default {
   },
   data(){
     return{
+      show: false,
       boardModalOpen: false
     }
   },

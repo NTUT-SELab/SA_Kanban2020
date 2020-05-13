0c1ddf5c1a278252e9106b22ad68587ec5051564
enchang <enchang1017@gmail.com>
2020-05-13 12:06:52 +0800

    modify board iu

--- SElab/KanbanFront/src/components/navBar.vue
+++ SElab/KanbanFront/src/components/navBar.vue
@@ -1,29 +1,25 @@
 <template>
 <div>
-  <vk-navbar>
+  <vk-navbar id="boardNav">
     <vk-navbar-nav slot="left">
       <vk-iconnav-item href="#" icon="home"></vk-iconnav-item>
     </vk-navbar-nav>
 
-    <vk-navbar-logo slot="center">Kanban</vk-navbar-logo>
+    <vk-navbar-logo id="nvaLogo" slot="center">Kanban</vk-navbar-logo>
 
     <vk-navbar-nav slot="right">
-      <vk-iconnav-item icon="plus" v-on:click="openBoardModal"></vk-iconnav-item>
-      <vk-button @click="show = true">New</vk-button>
+      <vk-iconnav-item id="navbarPlusIcon" icon="plus" @click="show = true"></vk-iconnav-item>
+      <vk-button id="navbarCreate" @click="show = true">New</vk-button>
     </vk-navbar-nav>
 
   </vk-navbar>
 
-  <div >
-      <modal-board v-model="boardModalOpen"></modal-board>
-  </div>
-
-  <vk-modal size="container" :show.sync="show">
+  <vk-modal center :show.sync="show">
     <div class="container">
         <form>
             <fieldset class="uk-fieldset">
-
-                <legend class="uk-legend">Create Board</legend>
+                <vk-modal-close @click="show = false"></vk-modal-close>
+                <legend id="modalLegend" class="uk-legend">Create Board</legend>
 
                 <div class="uk-margin">
                     <input class="uk-input" type="text" placeholder="Board Title">
@@ -33,7 +29,7 @@
                     <textarea class="uk-textarea" rows="5" placeholder="Board Description"></textarea>
                 </div>
                 <p v-vk-margin>
-                    <vk-button @click.prevent="show = false">close</vk-button>
+                    <vk-button type="primary" id="modalCreateButton" @click.prevent="show = false">Create</vk-button>
                 </p>
             </fieldset>
         </form>
@@ -45,15 +41,10 @@
 </template>
 
 <script>
-import ModalBoard from "./ModalBoard.vue";
 export default {
-  components: {
-    ModalBoard
-  },
   data(){
     return{
       show: false,
-      boardModalOpen: false
     }
   },
   methods:{
@@ -63,3 +54,35 @@ export default {
   }
 }
 </script>>
+
+<style scoped>
+  #boardNav{
+    background-color: #23a8fa !important;
+  }
+
+  #navbarCreate{
+    background-color: transparent;
+    color: white;
+    font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  }
+
+  #modalCreateButton{
+    color: white;
+    font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  }
+
+  #nvaLogo{
+    color: white;
+    font-size: 40px;
+    font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  }
+
+  #navbarPlusIcon{
+    color: white;
+  }
+
+  #modalLegend{
+    font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif, bold;
+  }
+
+</style>

6ecb0a4f200d71782b407a0b41ad75feca42145a
enchang <enchang1017@gmail.com>
2020-05-14 22:14:31 +0800

    modify "

--- SElab/src/main/java/ddd/kanban/adapter/controller/RESTBoardAdapter.java
+++ SElab/src/main/java/ddd/kanban/adapter/controller/RESTBoardAdapter.java
@@ -6,30 +6,41 @@ import ddd.kanban.usecase.board.create.CreateBoardInput;
 import ddd.kanban.usecase.board.create.CreateBoardOutput;
 import ddd.kanban.usecase.board.create.CreateBoardUseCase;
 import ddd.kanban.usecase.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 public class RESTBoardAdapter {
     private BoardRepository boardRepository;
     private DomainEventBus domainEventBus;
 
+    @Autowired
     public RESTBoardAdapter(BoardRepository boardRepository, DomainEventBus domainEventBus){
         this.boardRepository = boardRepository;
         this.domainEventBus = domainEventBus;
     }
 
-    public BoardDTO createBoard(BoardDTO requestBoardDTO) {
-        String title = requestBoardDTO.getTitle();
-        String description = requestBoardDTO.getDescription();
+    @RestController
+    @RequestMapping(value = "/board", produces = MediaType.APPLICATION_JSON_VALUE)
+    public class SpringBoardController {
 
-        CreateBoardUseCase createBoardUseCase = new CreateBoardUseCase(boardRepository, domainEventBus);
-        CreateBoardInput createBoardInput = new CreateBoardInput(title, description);
-        CreateBoardOutput createBoardOutput = new CreateBoardOutput();
-        createBoardUseCase.execute(createBoardInput, createBoardOutput);
+        @PostMapping
+        public ResponseEntity<BoardDTO> createBoard(@RequestBody BoardDTO createBoardInputBody) {
 
-        BoardDTO returnBoardDTO = new BoardDTO();
-        returnBoardDTO.setId(createBoardOutput.getBoardId());
-        returnBoardDTO.setTitle(createBoardOutput.getBoardTitle());
-        returnBoardDTO.setDescription(createBoardOutput.getBoardDescription());
-        return returnBoardDTO;
+            CreateBoardUseCase createBoardUseCase = new CreateBoardUseCase(boardRepository, domainEventBus);
+            CreateBoardInput createBoardInput = new CreateBoardInput(createBoardInputBody.getTitle(), createBoardInputBody.getDescription());
+            CreateBoardOutput createBoardOutput = new CreateBoardOutput();
+
+            createBoardUseCase.execute(createBoardInput, createBoardOutput);
+
+            BoardDTO boardDTO = new BoardDTO();
+
+            return ResponseEntity.ok().body(boardDTO);
+        }
     }
 }

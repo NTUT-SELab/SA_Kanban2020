f64f3d46d15133fbbd91b01b8f99f67310e53eca
enchang <enchang1017@gmail.com>
2020-05-22 20:56:32 +0800

    modify spring config

--- SElab/src/main/java/ddd/kanban/application/config/DomainEventBusConfig.java
+++ SElab/src/main/java/ddd/kanban/application/config/DomainEventBusConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 public class DomainEventBusConfig {
 
     @Bean
-    public DomainEventBus DomainEventBusFactory() {
+    public DomainEventBus domainEventBus() {
         return  new DomainEventBus();
     }
 }

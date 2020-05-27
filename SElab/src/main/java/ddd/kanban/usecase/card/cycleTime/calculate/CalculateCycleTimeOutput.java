ba4e87ba33dc15646437c990a5651ca59cdf92c0
Kai Huang <kai.huang4b6169@gmail.com>
2020-05-27 21:47:37 +0800

    add CalculateCycleTimeUsecase

---
+++ SElab/src/main/java/ddd/kanban/usecase/card/cycleTime/calculate/CalculateCycleTimeOutput.java
@@ -0,0 +1,13 @@
+package ddd.kanban.usecase.card.cycleTime.calculate;
+
+public class CalculateCycleTimeOutput {
+    CycleTime cycleTime;
+
+    public void setCycleTime(CycleTime cycleTime) {
+        this.cycleTime = cycleTime;
+    }
+
+    public CycleTime getCycleTime() {
+        return cycleTime;
+    }
+}
---
+++ SElab/src/main/java/ddd/kanban/usecase/card/cycleTime/calculate/CalculateCycleTimeUseCase.java
@@ -0,0 +1,77 @@
+package ddd.kanban.usecase.card.cycleTime.calculate;
+
+import ddd.kanban.domain.model.FlowEvent;
+import ddd.kanban.domain.model.card.event.CardUnCommitted;
+import ddd.kanban.domain.model.workflow.Lane;
+import ddd.kanban.usecase.repository.FlowEventRepository;
+import ddd.kanban.usecase.repository.WorkflowRepository;
+import ddd.kanban.usecase.workflow.entity.ColumnEntity;
+import ddd.kanban.usecase.workflow.entity.ColumnEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CalculateCycleTimeUseCase {
+
+    private FlowEventRepository flowEventRepository;
+    private WorkflowRepository workflowRepository;
+
+    public CalculateCycleTimeUseCase(WorkflowRepository workflowRepository, FlowEventRepository flowEventRepository) {
+        this.workflowRepository = workflowRepository;
+        this.flowEventRepository = flowEventRepository;
+    }
+
+    public void execute(CalculateCycleTimeInput calculateCycleTimeInput, CalculateCycleTimeOutput calculateCycleTimeOutput){
+        List<FlowEventPair> flowEventPairs;
+        List<String> laneIntervalIds;
+        CycleTime cycleTime;
+
+        flowEventPairs = getCardFlowEventPairs(calculateCycleTimeInput.getCardId());
+        laneIntervalIds = getLaneIntervalIds(calculateCycleTimeInput.getWorkflowId(), calculateCycleTimeInput.getBeginningLaneId(), calculateCycleTimeInput.getEndLaneId());
+        cycleTime = calculateCycleTime(flowEventPairs, laneIntervalIds);
+
+        calculateCycleTimeOutput.setCycleTime(cycleTime);
+    }
+
+
+    private List<FlowEventPair> getCardFlowEventPairs(String cardId) {
+        List<FlowEventPair> flowEventPairs = new ArrayList<>();
+        FlowEvent committed = null;
+        for(FlowEvent flowEvent: this.flowEventRepository.findAll()){
+            if (flowEvent.getCardId().equals(cardId)){
+                if (committed == null){
+                    committed = flowEvent;
+                }else{
+                    flowEventPairs.add(new FlowEventPair(committed, flowEvent));
+                    committed = null;
+                }
+            }
+        }
+        if (committed != null)
+            flowEventPairs.add(new FlowEventPair(committed, null));
+        return flowEventPairs;
+    }
+
+    private List<String> getLaneIntervalIds(String workflowId, String beginningLaneId, String endLaneId) {
+        List<String> laneIntervalIds = new ArrayList<>();
+        Boolean isInLaneInterval = false;
+
+        for(ColumnEntity laneEntity: this.workflowRepository.findById(workflowId).getLaneEntities()){
+            if (isInLaneInterval || laneEntity.getId() == beginningLaneId){
+                laneIntervalIds.add(laneEntity.getId());
+                isInLaneInterval = (laneEntity.getId() == endLaneId) ? false : true;
+            }
+        }
+        return laneIntervalIds;
+    }
+
+    private CycleTime calculateCycleTime(List<FlowEventPair> flowEventPairs, List<String> laneIntervalIds) {
+        long diff = 0;
+        for(FlowEventPair flowEventPair: flowEventPairs){
+            if (laneIntervalIds.contains(flowEventPair.getLaneId())){
+                diff += flowEventPair.getCycleTime().getMillisecond();
+            }
+        }
+        return new CycleTime(diff);
+    }
+}

4081c1ca8039e94e299d854c3abb790d6761f331
Allen <s9151427@gmail.com>
2020-06-03 14:27:53 +0800

    revise Spring-Boot Bean Dependency

--- SElab/src/main/java/ddd/kanban/application/config/DomainEventBusConfig.java
+++ SElab/src/main/java/ddd/kanban/application/config/DomainEventBusConfig.java
@@ -1,14 +1,17 @@
 package ddd.kanban.application.config;
 
 import ddd.kanban.domain.model.DomainEventBus;
+import ddd.kanban.usecase.handler.DomainEventHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DomainEventBusConfig {
 
+
     @Bean
-    public static DomainEventBus domainEventBus() {
-        return  new DomainEventBus();
+    public DomainEventBus domainEventBus() {
+        return new DomainEventBus();
     }
 }
--- SElab/src/main/java/ddd/kanban/application/config/RepositoryConfig.java
+++ SElab/src/main/java/ddd/kanban/application/config/RepositoryConfig.java
@@ -1,7 +1,9 @@
 package ddd.kanban.application.config;
 
 import ddd.kanban.adapter.repository.board.SqliteBoardRepository;
+import ddd.kanban.adapter.repository.workflow.InMemoryWorkflowRepository;
 import ddd.kanban.usecase.repository.BoardRepository;
+import ddd.kanban.usecase.repository.WorkflowRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,7 +11,12 @@ import org.springframework.context.annotation.Configuration;
 public class RepositoryConfig {
 
     @Bean
-    public static BoardRepository boardRepository() {
+    public BoardRepository boardRepository() {
         return new SqliteBoardRepository();
+    }
+
+    @Bean
+    public WorkflowRepository workflowRepository() {
+        return new InMemoryWorkflowRepository();
     }
 }
